8560786f9bd5f1821f08721268712620b34e725f
Oleksandra Drapushko <oleksandra.drp@gmail.com>
2025-10-23 16:51:49 +0200

    lesson

---
+++ Lessons/_1/assets/script.js
@@ -0,0 +1,35 @@
+// let greetings = "Hi!"
+// let numberA = 0
+// let numberB = 2
+// let myNumber = "1"
+
+// let greetNumber=  greetings + numberA
+
+// let sum = numberA + numberB
+// let sumB = numberB + myNumber
+
+// console.log(greetings);
+// console.log(numberA);
+// console.log(greetNumber);
+// console.log(sum);
+
+// console.log(sumB);
+
+// let number = 0
+
+// number  = number + 1
+// number  = number + 1
+// number  = number + 1
+// number  = number + 1
+// number  = number + 1
+
+// console.log(number)
+
+const BUTTON = document.getElementById("button")
+
+BUTTON.addEventListener("click", () => {
+    
+console.log("ciao")
+}
+) 
+
