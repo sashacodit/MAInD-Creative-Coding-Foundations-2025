41cfbd3ec38d1fb89282c21bc346f8856e0cd850
Oleksandra Drapushko <oleksandra.drp@gmail.com>
2025-11-02 19:50:58 +0100

    switcher done

---
+++ Assignments/Assignment-01/assets/script.js
@@ -0,0 +1,41 @@
+const addButton = document.getElementById('add-btn');
+
+const listButton = document.getElementById('list-view-switch');
+console.log(listButton);
+const cardButton = document.getElementById('card-view-switch');
+console.log(cardButton);
+
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list-container');
+
+listButton.addEventListener('click', () => {
+    console.log ('List button pressed!');
+
+    taskList.classList.remove('card-view');
+    taskList.classList.add('list-view');
+
+    listButton.classList.add('active');
+    cardButton.classList.remove('active');
+
+})
+
+
+cardButton.addEventListener('click', () => {
+    console.log ('Card button pressed!');
+
+    taskList.classList.remove('list-view');
+    taskList.classList.add('card-view');
+    cardButton.classList.add('active');
+    listButton.classList.remove('active');
+
+})
+
+addButton.addEventListener('click', () => {
+    console.log('Add button pressed!');
+    const inputValue = taskInput.value;
+    const listElement = document.createElement('li');
+    listElement.innerHTML = inputValue;
+    taskList.appendChild(listElement);
+    taskInput.value = '';
+
+    })
