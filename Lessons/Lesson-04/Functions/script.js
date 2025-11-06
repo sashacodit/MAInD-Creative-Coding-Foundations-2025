ecde2385cc35badcba71a80332712edd47e7449b
Oleksandra Drapushko <oleksandra.drp@gmail.com>
2025-11-06 12:08:09 +0100

    lesson

---
+++ Lessons/Lesson-04/Functions/script.js
@@ -0,0 +1,12 @@
+function printInfo(name, surname,  course) 
+{
+    console.log("Name: "+ name + " " + surname + ",  course" + course);
+}
+
+function printGrades(name, surname, grades)
+{
+   console.log("Name: " + name + ", Surname: " + surname + ",  grades" + course);
+}
+ 
+
+printInfo("Andrei", "Ioffe", "Creative Coding Foundations");
